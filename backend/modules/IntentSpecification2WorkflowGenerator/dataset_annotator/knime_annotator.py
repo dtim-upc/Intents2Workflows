aa3665c56b536708c562b76a1b2153896c5a6c43
adriaag <adria.aumatell@estudiantat.upc.edu>
2025-02-17 09:29:41 +0100

    Update dataset annotator

--- backend/modules/IntentSpecification2WorkflowGenerator/dataset_annotator/knime_annotator.py
+++ backend/modules/IntentSpecification2WorkflowGenerator/dataset_annotator/knime_annotator.py
@@ -4,6 +4,8 @@ import sys
 from os import path
 
 import pandas as pd
+import scipy.stats as stats
+import numpy as np
 
 sys.path.append(os.path.abspath('..'))
 
@@ -13,7 +15,7 @@ from common import *
 def add_dataset_info(dataset_path, graph, label):
     dataset_node = ab.term(path.basename(dataset_path))
     graph.add((dataset_node, RDF.type, dmop.TabularDataset))
-    dataset = pd.read_csv(dataset_path, encoding='latin', delimiter=";")
+    dataset = pd.read_csv(dataset_path, encoding='utf-8', delimiter=",")
     add_csv_info(dataset_path, dataset, dataset_node, graph)
     add_column_info(dataset_path, dataset, dataset_node, graph, label)
 
@@ -64,10 +66,10 @@ def has_nulls(column):
 
 
 def is_categorical(column_type, column):
-    if column_type != 'object' and column_type != 'int64':
+    if get_column_type(column_type, column) in [dmop.Integer, dmop.Float]:
         return False
     else:
-        return column.nunique() < column.size / 3
+        return True
 
 
 def is_unique(column_type, column):
@@ -75,10 +77,51 @@ def is_unique(column_type, column):
         return False
     else:
         return column.nunique() == column.size
+    
+
+def is_normal(column_type, column):
+    if column_type in [dmop.Float]:
+        clean_col = column.dropna()
+        _, p_value = stats.shapiro(x=clean_col)
+        if p_value < 0.05:
+            sk = stats.skew(clean_col)
+            ku = stats.kurtosis(clean_col)
+            if abs(sk)<1 and abs(ku)<2:
+                return True
+            else:
+                return False
+        else:
+            return True
+        
+
+def check_outliers(column_type, column):
+    if column_type in [dmop.Float]:
+        clean_col = column.dropna()
+
+        median = np.median(clean_col)
+        mad = stats.median_abs_deviation(clean_col)
+        mad_constant = 1.4826 
+        modified_z_scores = np.abs((clean_col - median) / (mad_constant * mad))
+        
+        outliers = modified_z_scores > 3.0
+        outlier_indices = clean_col[outliers]
+
+        if len(outlier_indices) > 0.25*len(clean_col):
+            return False
+        else:
+            return True
+        
+
+def get_percentage_of_missing_rows(dataset):
+    return round(dataset.isna().any(axis=1).sum()/dataset.shape[0], 3)
 
 
 def add_column_info(dataset_path, dataset, dataset_node, graph, label):
     print('\tAdding column info:')
+    missing_percentage = get_percentage_of_missing_rows(dataset)
+    if missing_percentage != 0.0:
+        graph.add((dataset_node, dmop.missingvaluesPercentage, Literal(missing_percentage)))
+    graph.add((dataset_node, dmop.containsOutliers, Literal(False)))
     for col in dataset.columns:
         col_type = dataset[col].dtype.name
         col_node = ab.term(f'{path.basename(dataset_path)}/{col}')
@@ -91,11 +134,22 @@ def add_column_info(dataset_path, dataset, dataset_node, graph, label):
         unique = is_unique(col_type, dataset[col])
         nulls = has_nulls(dataset[col])
         position = dataset.columns.get_loc(col)
+        normality = is_normal(column_type, dataset[col])
+        outliers = check_outliers(column_type, dataset[col])
 
         graph.add((col_node, dmop.hasDataPrimitiveTypeColumn, column_type))
         graph.add((col_node, dmop.isCategorical, Literal(categorical)))
         graph.add((col_node, dmop.isUnique, Literal(unique)))
         graph.add((col_node, dmop.containsNulls, Literal(nulls)))
+        if normality:
+            graph.add((col_node, dmop.isNormalDistribution, Literal(normality)))
+            graph.add((dataset_node, dmop.isNormallyDistributed, Literal(normality)))
+        if outliers:
+            graph.add((col_node, dmop.hasOutliers, Literal(True)))
+            graph.remove((dataset_node, dmop.containsOutliers, Literal(False)))
+            graph.add((dataset_node, dmop.containsOutliers, Literal(True)))
+        else:
+            graph.add((col_node, dmop.hasOutliers, Literal(False)))
         if label != "" and col == label:  # Detect label attribute (if indicated) and annotate it
             graph.add((col_node, dmop.isFeature, Literal(False)))
             graph.add((col_node, dmop.isLabel, Literal(True)))
@@ -103,7 +157,7 @@ def add_column_info(dataset_path, dataset, dataset_node, graph, label):
             graph.add((col_node, dmop.isFeature, Literal(True)))
             graph.add((col_node, dmop.isLabel, Literal(False)))
         graph.add((col_node, dmop.hasPosition, Literal(position)))
-        print(f'\t\t{col}: {column_type} - {categorical=} - {unique=} - {position=} - {nulls=}')
+        print(f'\t\t{col}: {column_type} - {categorical=} - {unique=} - {position=} - {nulls=} - {normality=} - {outliers=}')
 
 
 def read_graph(urls):
