7148978fef4e18bb5a262c078126f4520080cca0
adriaag <adria.aumatell@estudiantat.upc.edu>
2025-02-17 09:46:20 +0100

    Refactor of modified files

--- backend/modules/IntentSpecification2WorkflowGenerator/api/api_main.py
+++ backend/modules/IntentSpecification2WorkflowGenerator/api/api_main.py
@@ -8,9 +8,10 @@ import proactive
 from flask import Flask, request, send_file
 from flask_cors import CORS
 
+#TODO cahnge import names to the adecuate files
 from api.functions import *
-from pipeline_translator.pipeline_translator import translate_graph_folder, translate_graph
-from dataset_annotator.annotator import annotate_dataset
+from pipeline_translator.general_pipeline_translator import translate_graph_folder, translate_graph
+from dataset_annotator.knime_annotator import annotate_dataset
 
 import requests
 import json
@@ -22,109 +23,100 @@ temporary_folder = os.path.abspath(r'./api/temp_files')
 if not os.path.exists(temporary_folder):
     os.makedirs(temporary_folder)
 
+@app.post('/annotate_dataset')
+def annotate_dataset_from_frontend():
+    path = request.json.get('path', '')
+    label = request.json.get('label', '')
+    print(path[path.rfind(os.path.sep) + 1:-4])
+    print(path.rfind(os.path.sep))
+    data_product_name = path[path.rfind(os.path.sep) + 1:-4]
+
+    # new_path = path[0:path.rfind("\\") + 1] + data_product_name + "_annotated.ttl"
+    new_path = temporary_folder + "/" + data_product_name + "_annotated.ttl"
+    annotate_dataset(path, new_path, label)
+
+    custom_ontology = get_custom_ontology(new_path)
+    datasets = {n.fragment: n for n in custom_ontology.subjects(RDF.type, dmop.TabularDataset)}
+    return {"ontology": custom_ontology.serialize(format="turtle"),
+            "data_product_uri": datasets[data_product_name + ".csv"]}
+
+
 @app.get('/problems')
 def get_problems():
     ontology_only_problems = get_custom_ontology_only_problems()
     problems = {n.fragment: n for n in ontology_only_problems.subjects(RDF.type, tb.Problem)}
     return problems
 
-
 @app.post('/abstract_planner')
 def run_abstract_planner():
     intent_graph = get_graph_xp()
 
-    intent_name = request.json.get('intent_name', '')
-    dataset = request.json.get('dataset', '')
-    problem = request.json.get('problem', '')
+    data = request.json
+    intent_name = data.get('intent_name', '')
+    dataset = data.get('dataset', '')
+    task = data.get('problem', '')
+    algorithm = data.get('algorithm', '')
+    exposed_parameters = data.get('exposed_parameters', '')
+    percentage = data.get('preprocessing_percentage', '')
     ontology = Graph().parse(data=request.json.get('ontology', ''), format='turtle')
 
     intent_graph.add((ab.term(intent_name), RDF.type, tb.Intent))
     intent_graph.add((ab.term(intent_name), tb.overData, URIRef(dataset)))
-    intent_graph.add((ab.term(intent_name), tb.tackles, URIRef(problem)))
+    intent_graph.add((URIRef(task), tb.tackles, ab.term(intent_name)))
+    if algorithm != "":
+        intent_graph.add((ab.term(intent_name), tb.specifies, cb.term(algorithm)))
+
+    for exp_param in exposed_parameters:
+        param_val = list(exp_param.values())[0]
+        param = list(exp_param.keys())[0]
+        intent_graph.add((ab.term(intent_name), tb.specifiesValue, Literal(param_val)))
+        intent_graph.add((Literal(param_val), tb.forParameter, URIRef(param)))
+
+    intent_graph.add((ab.term(intent_name), tb.has_component_threshold, Literal(percentage)))
 
     intent = intent_graph
+
     abstract_plans, algorithm_implementations = abstract_planner(ontology, intent)
+
     return {"abstract_plans": abstract_plans, "intent": intent.serialize(format="turtle"),
-            "algorithm_implementations": algorithm_implementations}
-
-
-def convert_strings_to_uris(obj):
-    if isinstance(obj, list):  # If the object is a list
-        return [convert_strings_to_uris(item) for item in obj]  # Recursively process each item in the list
-    elif isinstance(obj, dict):  # If the object is a dictionary (only the "first" level)
-        # Recursively process each value in the dictionary + transform key to URI
-        return {URIRef(key): convert_strings_to_uris(value) for key, value in obj.items()}
-    else:
-        return URIRef(obj)  # Convert non-list, non-dictionary values to URIs
-
+        "algorithm_implementations": algorithm_implementations}
 
 @app.post('/logical_planner')
 def run_logical_planner():
+
     plan_ids = request.json.get('plan_ids', '')
     intent_json = request.json.get('intent_graph', '')
     algorithm_implementations = request.json.get('algorithm_implementations', '')
     ontology = Graph().parse(data=request.json.get('ontology', ''), format='turtle')
 
-    # The algorithms come from the frontend in String format. We need to change them back to URIRefs
-    algorithm_implementations_uris = convert_strings_to_uris(algorithm_implementations)
-
     intent = Graph().parse(data=intent_json, format='turtle')
 
     impls = [impl
-             for alg, impls in algorithm_implementations_uris.items() if str(alg) in plan_ids
+             for alg, impls in algorithm_implementations.items() if str(alg) in plan_ids
              for impl in impls]
 
     workflow_plans = workflow_planner(ontology, impls, intent)
-    ########### ORIGINAL CODE
-    # logical_plans = logical_planner(ontology, workflow_plans)
-    # return logical_plans
 
-    ########### EXTREMEXP CODE
-    logical_plans, extremexp_workflows, tasks = logical_planner_extremexp(ontology, workflow_plans)
-    # Retrieving the intent name
-    query_template = """ 
-                        PREFIX ab: <https://extremexp.eu/ontology/abox#>
-                        PREFIX tb: <https://extremexp.eu/ontology/tbox#>
-                        SELECT ?subject
-                        WHERE {
-                          ?subject a tb:Intent .
-                        } """
-    results = intent.query(query_template)
-    intent_name = ""
-    for row in results:
-        intent_name = str(row["subject"][row["subject"].find('#') + 1:])
+    logical_plans = logical_planner(
+        ontology, workflow_plans)
 
-    for workflow in extremexp_workflows:
-        task_implementations = workflow.get('task_implementations', {})
-        for key, value in task_implementations.items():
-            updated_value = value.replace("intent_name", intent_name)
-            task_implementations[key] = updated_value
-        workflow['task_implementations'] = task_implementations
-
-    json_data = {
-        "intent_name": intent_name,
-        "tasks": tasks,
-        "workflows": extremexp_workflows
-    }
-
-    # Write data to JSON file
-    with open(os.path.join(temporary_folder, "intent_to_dsl.json"), 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
+    #TODO Logical planner from extremeXP?
 
     return logical_plans
 
+@app.post('/workflow_plans/knime/<plan_id>')
+def download_knime(plan_id):
+    plan_graph = Graph().parse(data=request.json.get("graph", ""), format='turtle')
+    ontology = Graph().parse(data=request.json.get('ontology', ''), format='turtle')
 
-@app.post('/intent-to-dsl')
-def download_file():
-    # Define the path where the file is stored
-    file_path = os.path.join(temporary_folder, "intent_to_dsl.json")
+    file_path = os.path.join(temporary_folder, f'{plan_id}.ttl')
+    plan_graph.serialize(file_path, format='turtle')
 
-    # Check if the file exists
-    if not os.path.exists(file_path):
-        abort(404, description="File not found")
+    knime_file_path = file_path[:-4] + '.knwf'
+    translate_graph(ontology, file_path, knime_file_path)
 
-    # Send the file for download
-    return send_file(file_path, as_attachment=True)
+    return send_file(knime_file_path, as_attachment=True)
+
 
 @app.post('/workflow_plans/knime/all')
 def download_all_knime():
@@ -151,38 +143,17 @@ def download_all_knime():
     compress(knime_folder, knime_folder + '.zip')
     return send_file(knime_folder + '.zip', as_attachment=True)
 
+@app.post('/intent-to-dsl')
+def download_file():
+    # Define the path where the file is stored
+    file_path = os.path.join(temporary_folder, "intent_to_dsl.json")
 
-@app.post('/workflow_plans/knime')
-def download_knime():
-    plan_graph = Graph().parse(data=request.json.get("graph", ""), format='turtle')
-    ontology = Graph().parse(data=request.json.get('ontology', ''), format='turtle')
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        abort(404, description="File not found")
 
-    file_path = os.path.join(temporary_folder, f'{uuid.uuid4()}.ttl')
-    plan_graph.serialize(file_path, format='turtle')
-
-    knime_file_path = file_path[:-4] + '.knwf'
-    translate_graph(ontology, file_path, knime_file_path)
-
-    return send_file(knime_file_path, as_attachment=True)
-
-
-@app.post('/annotate_dataset')
-def annotate_dataset_from_frontend():
-    path = request.json.get('path', '')
-    label = request.json.get('label', '')
-    print(path[path.rfind(os.path.sep) + 1:-4])
-    print(path.rfind(os.path.sep))
-    data_product_name = path[path.rfind(os.path.sep) + 1:-4]
-
-    # new_path = path[0:path.rfind("\\") + 1] + data_product_name + "_annotated.ttl"
-    new_path = temporary_folder + "/" + data_product_name + "_annotated.ttl"
-    annotate_dataset(path, new_path, label)
-
-    custom_ontology = get_custom_ontology(new_path)
-    datasets = {n.fragment: n for n in custom_ontology.subjects(RDF.type, dmop.TabularDataset)}
-    return {"ontology": custom_ontology.serialize(format="turtle"),
-            "data_product_uri": datasets[data_product_name + ".csv"]}
-
+    # Send the file for download
+    return send_file(file_path, as_attachment=True)
 
 ################################# INTEGRATION FUNCTIONS
 
--- backend/modules/IntentSpecification2WorkflowGenerator/api/functions.py
+++ backend/modules/IntentSpecification2WorkflowGenerator/api/functions.py
@@ -1,167 +1,11 @@
 import zipfile
+import sys
+import os
 
 from rdflib.term import Node
 
-from pipeline_generator.pipeline_generator import *
-
-
-def abstract_planner(ontology: Graph, intent: Graph) -> Tuple[
-    Dict[Node, Dict[Node, List[Node]]], Dict[Node, List[Node]]]:
-    dataset, problem, intent_params, intent_iri = get_intent_info(intent)
-
-    impls = get_potential_implementations(ontology, problem, [])
-
-    algs = [(next(ontology.objects(impl[0], tb.implements)),
-             (impl[0], RDF.type, tb.LearnerImplementation) in ontology) for impl in impls]
-
-    plans = {}
-    for alg, train in algs:
-        if train:
-            trainer = cb.term(alg.fragment + '-Train')
-            plans[alg] = {
-                cb.DataLoading: [cb.Partitioning],
-                cb.Partitioning: [trainer, alg],
-                trainer: [alg],
-                alg: [cb.DataStoring],
-                cb.DataStoring: []
-            }
-        else:
-            plans[alg] = {
-                cb.DataLoading: [alg],
-                alg: [cb.DataStoring],
-                cb.DataStoring: []
-            }
-
-    alg_plans = {alg: [] for alg, _ in algs}
-    for impl in impls:
-        alg_plans[next(ontology.objects(impl[0], tb.implements))].append(impl)
-
-    return plans, alg_plans
-
-
-def workflow_planner(ontology: Graph, implementations: List, intent: Graph, log: bool = False):
-    dataset, problem, intent_params, intent_iri = get_intent_info(intent)
-
-    if log:
-        print(f'Dataset: {dataset.fragment}')
-        print(f'Problem: {problem.fragment}')
-        print(f'Intent params: {intent_params}')
-        print('-------------------------------------------------')
-        print(implementations)
-
-
-    components = [
-        (c, impl, inputs)
-        for impl, inputs in implementations
-        for c in get_implementation_components(ontology, impl)
-    ]
-    workflow_order = 0
-
-    if log:
-        for component, implementation, inputs in components:
-            print(f'Component: {component.fragment} ({implementation.fragment})')
-            for im_input in inputs:
-                print(f'\tInput: {[x.fragment for x in im_input]}')
-        print('-------------------------------------------------')
-
-    split_components = [
-        cb.term('component-random_absolute_train_test_split'),
-        cb.term('component-random_relative_train_test_split'),
-        cb.term('component-top_k_absolute_train_test_split'),
-        cb.term('component-top_k_relative_train_test_split'),
-    ]
-
-    workflows = []
-
-    for component, implementation, inputs in tqdm(components, desc='Components', position=1):
-        if log:
-            print(f'Component: {component.fragment} ({implementation.fragment})')
-
-        shapes_to_satisfy = identify_data_io(ontology, inputs)
-        assert shapes_to_satisfy is not None and len(shapes_to_satisfy) > 0
-
-        if log:
-            print(f'\tData input: {[x.fragment for x in shapes_to_satisfy]}')
-
-        unsatisfied_shapes = [shape for shape in shapes_to_satisfy if
-                              not satisfies_shape(ontology, ontology, shape, dataset)]
-
-        available_transformations = {
-            shape: find_components_to_satisfy_shape(ontology, shape, only_learners=True)
-            for shape in unsatisfied_shapes
-        }
-        print("Variablity points:")
-        print(f'\tSplitComponents: {[x.fragment for x in split_components]}')
-        for shape, comps in available_transformations.items():
-            print(f'\t{shape.fragment}: {[x.fragment for x in comps]}')
-
-
-        if log:
-            print(f'\tUnsatisfied shapes: ')
-            for shape, comps in available_transformations.items():
-                print(f'\t\t{shape.fragment}: {[x.fragment for x in comps]}')
-
-        transformation_combinations = list(
-            enumerate(itertools.product(split_components, *available_transformations.values())))
-        # TODO - check if the combination is valid and whether further transformations are needed
-
-        if log:
-            print(f'\tTotal combinations: {len(transformation_combinations)}')
-
-        for i, transformation_combination in tqdm(transformation_combinations, desc='Transformations', position=0,
-                                                  leave=False):
-            if log:
-                print(
-                    f'\t\tCombination {i + 1} / {len(transformation_combinations)}: {[x.fragment for x in transformation_combination]}')
-
-            workflow_name = f'workflow_{workflow_order}_{intent_iri.fragment}_{uuid.uuid4()}'.replace('-', '_')
-            wg, w = build_workflow_train_test(workflow_name, ontology, dataset, component,
-                                              transformation_combination[0],
-                                              transformation_combination[1:])
-
-            wg.add((w, tb.createdFor, intent_iri))
-            wg.add((intent_iri, RDF.type, tb.Intent))
-
-            workflows.append(wg)
-            workflow_order += 1
-            if log:
-                print(f'\t\tWorkflow {workflow_order}: {w.fragment}')
-    return workflows
-
-
-def logical_planner(ontology: Graph, workflow_plans: List[Graph]):
-    logical_plans = {}
-    counter = {}
-    for workflow_plan in workflow_plans:
-        steps = list(workflow_plan.subjects(RDF.type, tb.Step))
-        step_components = {step: next(workflow_plan.objects(step, tb.runs)) for step in steps}
-        step_next = {step: list(workflow_plan.objects(step, tb.followedBy)) for step in steps}
-        logical_plan = {
-            step_components[step]: [step_components[s] for s in nexts] for step, nexts in step_next.items()
-        }
-        main_component = next(
-            comp for comp in logical_plan.keys() if logical_plan[comp] == [cb.term('component-csv_local_writer')])
-        if (main_component, RDF.type, tb.ApplierImplementation) in ontology:
-            options = list(ontology.objects(main_component, tb.hasLearner))
-            main_component = next(o for o in options if (None, None, o) in workflow_plan)
-        if main_component not in counter:
-            counter[main_component] = 0
-        plan_id = (f'{main_component.fragment.split("-")[1].replace("_", " ").replace(" learner", "").title()} '
-                   f'{counter[main_component]}')
-        counter[main_component] += 1
-        logical_plans[plan_id] = {"logical_plan": logical_plan, "graph": workflow_plan.serialize(format="turtle")}
-
-    return logical_plans
-
-
-def compress(folder: str, destination: str) -> None:
-    with zipfile.ZipFile(destination, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, _, files in os.walk(folder):
-            for file in files:
-                file_path = os.path.join(root, file)
-                archive_path = os.path.relpath(file_path, folder)
-                zipf.write(file_path, arcname=os.path.join(os.path.basename(folder), archive_path))
-
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from pipeline_generator.optimized_pipeline_generator import *
 
 def get_custom_ontology(path):
     graph = get_graph_xp()
@@ -190,13 +34,124 @@ def get_custom_ontology_only_problems():
     DeductiveClosure(OWLRL_Semantics).expand(graph)
     return graph
 
+def connect_algorithms(ontology, algos_list):
+    impls_algos = {imp : algo + "-Train" if "learner" in imp.fragment else algo
+                   for algo in algos_list for (imp, _) in get_all_implementations(ontology, None, algo)}
 
-def logical_planner_extremexp(ontology: Graph, workflow_plans: List[Graph]):
+    linked_impls = {}
+
+    impls_list = list(impls_algos.keys())
+    
+    for i in range(len(impls_list) - 1):
+        preceding_impl = impls_list[i]
+        following_impls = impls_list[i + 1:]
+
+        out_specs = get_implementation_output_specs(ontology, preceding_impl)
+        out_spec_set = {out_sp for out_spec in out_specs for out_sp in out_spec}
+
+        
+        for following_impl in following_impls:
+
+            in_specs = get_implementation_input_specs(ontology, following_impl)
+            in_spec_set = {in_sp for in_spec in in_specs for in_sp in in_spec}
+
+            if out_spec_set & in_spec_set:
+                
+                preceding_impl_key = impls_algos[preceding_impl]
+                following_impl_key = impls_algos[following_impl]
+
+                linked_impls.setdefault(preceding_impl_key, [])
+                if following_impl_key not in linked_impls[preceding_impl_key]:
+                    linked_impls[preceding_impl_key].append(following_impl_key)
+    
+    linked_impls[following_impl_key] = []
+
+    return linked_impls
+
+
+
+def abstract_planner(ontology: Graph, intent: Graph) -> Tuple[
+    Dict[Node, Dict[Node, List[Node]]], Dict[Node, List[Node]]]:
+
+    dataset, task, algorithm, intent_iri = get_intent_info(intent)
+
+    algs = [algorithm] if algorithm is not None else get_algorithms_from_task(ontology, task)
+
+    impls = get_potential_implementations(ontology, task, algorithm)
+
+    algs_shapes = {}
+    alg_plans = {alg: [] for alg in algs}
+    for impl in impls:
+        alg = next(ontology.objects(impl[0], tb.implements)), 
+        (impl[0], RDF.type, tb.Implementation) in ontology and (tb.ApplierImplementation not in ontology.objects(impl[0], RDF.type))
+
+        algs_shapes[alg[0]] = impl[1::][0][0]
+
+        alg_plans[alg[0]].append(impl)
+    
+    plans = {}
+    for alg in algs:
+        if cb.TrainTabularDatasetShape in algs_shapes[alg]:
+            plans[alg] = connect_algorithms(ontology, [cb.DataLoading, cb.Partitioning, alg, cb.DataStoring])
+        else:
+            plans[alg] = connect_algorithms(ontology, [cb.DataLoading, alg])
+
+    return plans, alg_plans
+
+def workflow_planner(ontology: Graph, implementations: List, intent: Graph):
+    dataset, task, algorithm, intent_iri = get_intent_info(intent)
+
+    component_threshold = float(next(intent.objects(intent_iri, tb.has_component_threshold), None))
+
+    components = [
+        (c, impl, inputs)
+        for impl, inputs in implementations
+        for c in get_implementation_components(ontology, impl)
+    ]
+    workflow_order = 0
+
+    workflows = []
+
+    for component, implementation, inputs in tqdm(components, desc='Components', position=1):
+        shapes_to_satisfy = identify_data_io(ontology, inputs)
+        assert shapes_to_satisfy is not None and len(shapes_to_satisfy) > 0
+
+        unsatisfied_shapes = [shape for shape in shapes_to_satisfy if
+                              not satisfies_shape(ontology, ontology, shape, dataset)]
+
+        available_transformations = {
+            shape: find_components_to_satisfy_shape(ontology, shape, exclude_appliers=True)
+            for shape in unsatisfied_shapes
+        }
+
+        for transformation, methods in available_transformations.items():
+            best_components = get_best_components(ontology, task, methods, dataset, component_threshold/100.0)
+
+            available_transformations[transformation] = list(best_components.keys())
+
+
+        transformation_combinations = list(
+            enumerate(itertools.product(*available_transformations.values())))
+        # TODO - check if the combination is valid and whether further transformations are needed
+
+        for i, transformation_combination in tqdm(transformation_combinations, desc='Transformations', position=0,
+                                                  leave=False):
+            workflow_name = f'workflow_{workflow_order}_{intent_iri.fragment}_{uuid.uuid4()}'.replace('-', '_')
+            wg, w = build_general_workflow(workflow_name, ontology, dataset, component,
+                                           transformation_combination, intent) 
+
+            wg.add((w, tb.generatedFor, intent_iri))
+            wg.add((intent_iri, RDF.type, tb.Intent))
+
+            workflows.append(wg)
+            workflow_order += 1
+    return workflows
+
+
+def logical_planner(ontology: Graph, workflow_plans: List[Graph]):
     logical_plans = {}
+    mapper = {}
     counter = {}
-    tasks = []
-    extremexp_workflows = []
-    workflow_counter = 0
     for workflow_plan in workflow_plans:
         steps = list(workflow_plan.subjects(RDF.type, tb.Step))
         step_components = {step: next(workflow_plan.objects(step, tb.runs)) for step in steps}
@@ -204,14 +159,9 @@ def logical_planner_extremexp(ontology: Graph, workflow_plans: List[Graph]):
         logical_plan = {
             step_components[step]: [step_components[s] for s in nexts] for step, nexts in step_next.items()
         }
-
-        workflow_name = "Workflow_" + str(workflow_counter)
-        workflow_counter += 1
-        extremexp_workflow = generate_extremexp_workflow(ontology, logical_plan, tasks, workflow_name)
-        extremexp_workflows.append(extremexp_workflow)
-
-        main_component = next(
-            comp for comp in logical_plan.keys() if logical_plan[comp] == [cb.term('component-csv_local_writer')])
+        main_component = next((comp for comp in logical_plan.keys() 
+                      if logical_plan[comp] == [cb.term('component-csv_local_writer')] 
+                      or logical_plan[comp] == []), None)
         if (main_component, RDF.type, tb.ApplierImplementation) in ontology:
             options = list(ontology.objects(main_component, tb.hasLearner))
             main_component = next(o for o in options if (None, None, o) in workflow_plan)
@@ -220,51 +170,15 @@ def logical_planner_extremexp(ontology: Graph, workflow_plans: List[Graph]):
         plan_id = (f'{main_component.fragment.split("-")[1].replace("_", " ").replace(" learner", "").title()} '
                    f'{counter[main_component]}')
         counter[main_component] += 1
-        logical_plans[plan_id] = {"logical_plan": logical_plan, "graph": workflow_plan.serialize(format="turtle")}
+        logical_plans[plan_id] = logical_plan
+        mapper[plan_id] = workflow_plan
 
-    tasks = list(dict.fromkeys(tasks))  # remove duplicates
+    return logical_plans, mapper
 
-    return logical_plans, extremexp_workflows, tasks
-
-
-def generate_extremexp_workflow(ontology, logical_plan, tasks, workflow_name):
-    workflow = {}
-    workflow["workflow_name"] = workflow_name
-    task_implementations = {}
-    query_template = """ PREFIX tb: <https://extremexp.eu/ontology/tbox#>
-                                SELECT ?implementation ?implements
-                                WHERE {{
-                                    <{key}> tb:hasImplementation ?implementation .
-                                    ?implementation tb:implements ?implements .
-                                }} """
-
-    if len(tasks) == 0: # Build task array
-        for key in logical_plan.keys():
-            query_execute = query_template.format(key=key)
-            results = ontology.query(query_execute)
-            for row in results:
-                if "learner" in row.implementation:
-                    tasks.append("ModelTrain")
-                elif "predictor" in row.implementation:
-                    tasks.append("ModelPredict")
-                else:
-                    tasks.append(row.implements[row.implements.find('#') + 1:])
-
-    for key in logical_plan.keys():
-        query_execute = query_template.format(key=key)
-        results = ontology.query(query_execute)
-        for row in results:
-            if "applier" not in key:
-                task = row.implements[row.implements.find('#') + 1:]
-                if "learner" in row.implementation:
-                    task = "ModelTrain"
-                elif "predictor" in row.implementation:
-                    task = "ModelPredict"
-                task_implementations[task] = "tasks/intent_name/" + key[key.find('-') + 1:] + ".py"
-            # print(f"Key: {key}, implementation: {row.implementation}, implements: {row.implements}")
-
-    workflow["task_implementations"] = task_implementations
-    workflow["experiment_space_name"] = "S_" + workflow_name
-    workflow["experiment_space"] = []
-    return workflow
-
+def compress(folder: str, destination: str) -> None:
+    with zipfile.ZipFile(destination, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                archive_path = os.path.relpath(file_path, folder)
+                zipf.write(file_path, arcname=os.path.join(os.path.basename(folder), archive_path))
