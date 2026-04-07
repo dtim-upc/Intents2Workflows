1e62d03b2fe82319a9682f4009de2fc6da7a28b2
adriaag <99830362+adriaag@users.noreply.github.com>
2026-04-07 09:03:04 +0200

    iframe test

---
+++ frontend/src/pages/IframeTest.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="height:100vh">
+    Aquí tenim l'iframe
+    <iframe
+      src="http://localhost:9000"
+      style="width:90%; height:90%; border:none"
+      sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox allow-modals"
+    />
+  </div>
+</template>
